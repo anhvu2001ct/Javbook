7a7d38deb6c9684a306452e542aa483d3cab407d
nganhvu <nganhvu2161@gmail.com>
2021-11-03 00:22:22 +0700

    .

--- src/java/com/group1/test/nganhvu/DeleteCookie.java
+++ src/java/com/group1/test/nganhvu/DeleteCookie.java
@@ -3,6 +3,8 @@ package com.group1.test.nganhvu;
 import com.group1.controller.BaseServlet;
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,11 +12,14 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Anh Vu Nguyen {@literal <nganhvu>}
  */
-//@WebServlet("/test/nganhvu/Javbook.png/")
-public class DefaultServlet extends BaseServlet {
+@WebServlet("/test/nganhvu/delete/")
+public class DeleteCookie extends BaseServlet {
     @Override
     public void processGET(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("GET");
+        Cookie cookie = new Cookie("JBID", "");
+        cookie.setPath("/Javbook");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 
     @Override
