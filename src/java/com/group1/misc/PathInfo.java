8fc34199c06f0b82d0e29b32716084a20b74e66f
nganhvu <nganhvu2161@gmail.com>
2021-11-04 23:46:27 +0700

    .

--- src/java/com/group1/misc/PathInfo.java
+++ src/java/com/group1/misc/PathInfo.java
@@ -5,6 +5,8 @@
  */
 package com.group1.misc;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 /**
@@ -21,5 +23,26 @@ public class PathInfo {
         size = st.countTokens();
         path = new String[size];
         for (int i = 0; i < size; ++i) path[i] = st.nextToken();
+    }
+    
+    public static Map<String, String> getUrlParams(String url) {
+        Map<String, String> res = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(url, "&");
+        while (st.hasMoreTokens()) {
+            StringTokenizer param = new StringTokenizer(st.nextToken(), "=");
+            String key = param.nextToken();
+            res.put(key, param.nextToken());
+        }
+        return res;
+    }
+    
+    public static String toUrlParams(Map<String, String> params) {
+        StringBuilder res = new StringBuilder();
+        int cnt = 0;
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            res.append(entry.getKey()).append('=').append(entry.getValue());
+            if (++cnt < params.size()) res.append('&');
+        }
+        return res.toString();
     }
 }
